a571ef1e691f74bde77f9676863b90bdfc64cf16
Juan Isaza <juan@peaku.co>
2025-02-17 15:38:20 -0500

    bug

--- src/crashless/handler.py
+++ src/crashless/handler.py
@@ -488,7 +488,7 @@ def get_solution(environments, temp_patch_file, exc):
         )
 
     # there's no code
-    if code_fix.fixed_code is None:
+    if code_fix.fixed_code is None or code_fix.index is None:
         return Solution(
             not_found=False,
             file_path=code_fix.file_path,
